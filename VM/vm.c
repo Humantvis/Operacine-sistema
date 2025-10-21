7986bc02cba4e629c34522aec8d236f1fce0fd15
Deta0223 <deivydastamulis@gmail.com>
2025-10-21 23:14:05 +0300

    Command implementation
    Several are missing, because of missing features

--- VM/vm.c
+++ VM/vm.c
@@ -19,8 +19,8 @@ void destroyVM(VM* vm) {
 
 void runVM(RM* rm, VM* vm) {
     while (allowedToRun(rm, vm)) {
-        uint8_t* instr_ptr = vm->memory->codeMemory + (*(vm->cpu->ic)) * WORD_SIZE;
-        uint32_t instruction = *(uint32_t*)instr_ptr;
+        uint8_t* instr_ptr = vm->memory->codeMemory + (*(vm->cpu->ic));
+        uint8_t instruction = (*instr_ptr >> 3) & 0b00011111;
 
         executeInstruction(vm, instruction);
 
@@ -31,7 +31,7 @@ void runVM(RM* rm, VM* vm) {
 bool allowedToRun(RM* rm, VM* vm) {
     return rm->cpu->mountedVMID == vm->id;
 }
-void executeInstruction(VM* vm, uint32_t instruction) {
+void executeInstruction(VM* vm, uint8_t instruction) {
 
     uint8_t* mem = vm->memory->codeMemory;
     uint8_t* ic = vm->cpu->ic;
@@ -55,6 +55,7 @@ void executeInstruction(VM* vm, uint32_t instruction) {
             uint8_t x = mem[++(*ic)];
             uint8_t y = mem[++(*ic)];
             uint8_t z = mem[++(*ic)];
+            uint8_t w = mem[++(*ic)];
             vm->cpu->r[z] = vm->cpu->r[x] * vm->cpu->r[y];
             break;
         }
@@ -62,6 +63,7 @@ void executeInstruction(VM* vm, uint32_t instruction) {
             uint8_t x = mem[++(*ic)];
             uint8_t y = mem[++(*ic)];
             uint8_t z = mem[++(*ic)];
+            uint8_t w = mem[++(*ic)];
             vm->cpu->r[z] = vm->cpu->r[x] / vm->cpu->r[y];
             break;
         }
@@ -72,82 +74,106 @@ void executeInstruction(VM* vm, uint32_t instruction) {
             break;
         }
         case MRxyz: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            uint8_t z = mem[++(*ic)];
+            vm->cpu->r[z] = vm->memory->dataMemory[x * 16 + y];
             break;
         }
         case MWxyz: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            uint8_t z = mem[++(*ic)];
+            vm->memory->dataMemory[x * 16 + y] = vm->cpu->r[z];
             break;
         }
         case SMRxyz: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            uint8_t z = mem[++(*ic)];
+            vm->cpu->r[z] = vm->memory->sharedMemory[x * 16 + y];
             break;
         }
         case SMWxyz: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            uint8_t z = mem[++(*ic)];
+            vm->memory->sharedMemory[x * 16 + y] = vm->cpu->r[z];
             break;
         }
         case WAIT: {
+            //semaphores are not needed yet
             break;
         }
         case SIGNAL: {
+            //semaphores are not needed yet
             break;
         }
         case JMPxy: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            *(vm->cpu->ic) = x * 16 + y;
             break;
         }
         case JExy: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            if (vm->cpu->fr == 1) {
+                *(vm->cpu->ic) = x * 16 + y;
+            }
             break;
         }
         case JGxy: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            if ((vm->cpu->fr & FLAG_SF) == 0) 
+                *ic = x * 16 + y;
             break;
         }
         case JLxy: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+
+        if (vm->cpu->fr & FLAG_SF) 
+        *(vm->cpu->ic) = x * 16 + y;
             break;
         }
         case JLExy: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+
+        if ((vm->cpu->fr & FLAG_SF) || (vm->cpu->fr & FLAG_ZF))
+                *(vm->cpu->ic) = x * 16 + y;
             break;
         }
         case JC: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+            if (vm->cpu->fr & FLAG_CF) 
+                *(vm->cpu->ic) = x * 16 + y;
             break;
         }
         case JNC: {
+            uint8_t x = mem[++(*ic)];
+            uint8_t y = mem[++(*ic)];
+
+            if (!(vm->cpu->fr & FLAG_CF)) 
+                *(vm->cpu->ic) = x * 16 + y;
             break;
         }
         case DMARx: {
+            uint8_t x = mem[++(*ic)];
+            //need to implement instruction reading first
             break;
         }
         case DMAWx: {
+            uint8_t x = mem[++(*ic)];
+            //need to implement instruction writing first
             break;
         }
         case HALT: {
+            destroyVM(vm);
             break;
         }
     }
 }
-enum opCodes{
-    //0:
-    WAIT,
-    SIGNAL,
-    JNC,
-    JC,
-    HALT,
-    //1:
-    DMARx,
-    DMAWx,
-    //2:
-    CMPxy,
-    JMPxy,
-    JExy,
-    JNExy,
-    JGxy,
-    JGExy,
-    JLxy,
-    JLExy,
-    //3:
-    ADDxyz,
-    SUBxyz,
-    MRxyz,
-    MWxyz,
-    SMRxyz,
-    SMWxyz,
-    //4:
-    MULxyzw,
-    DIVxyzw
-};
--- VM/vm.h
+++ VM/vm.h
@@ -9,6 +9,11 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+#define FLAG_SF 0x01  // Sign flag
+#define FLAG_ZF 0x02  // Zero flag
+#define FLAG_CF 0x04  // Carry flag
+
+
 typedef struct VM {
     int id;
     VM_CPU* cpu;
@@ -21,4 +26,37 @@ void destroyVM(VM* vm);
 
 void runVM(RM* rm, VM* vm);
 
+void executeInstruction(VM* vm, uint8_t instruction);
+
 bool allowedToRun(RM* rm, VM* vm);
+
+enum opCodes{
+    //0:
+    WAIT,
+    SIGNAL,
+    JNC,
+    JC,
+    HALT,
+    //1:
+    DMARx,
+    DMAWx,
+    //2:
+    CMPxy,
+    JMPxy,
+    JExy,
+    JNExy,
+    JGxy,
+    JGExy,
+    JLxy,
+    JLExy,
+    //3:
+    ADDxyz,
+    SUBxyz,
+    MRxyz,
+    MWxyz,
+    SMRxyz,
+    SMWxyz,
+    //4:
+    MULxyzw,
+    DIVxyzw
+};
