96338d932f2203ec08e0f713a82ba1d72cb40552
Humantvis <Mantvydas.Skruodys@gmail.com>
2025-12-20 01:57:22 +0200

    fixed loader

--- KERNEL/loader.c
+++ KERNEL/loader.c
@@ -1,22 +1,58 @@
 #include "loader.h"
 #include <stdio.h>
+#include <string.h> // memcpy
 
 void loadProgram(RM* rm, ExternalMemory* externalMemory, int program) {
+
     int vm = -1;
-    for(int i = 0; i < MAX_PROCESESSES; i++) {
-        if(rm->memory->supervisorMemory->VMs[i] == false) {
+    for (int i = 0; i < MAX_PROCESESSES; i++) {
+        if (rm->memory->supervisorMemory->VMs[i] == false) {
             rm->memory->supervisorMemory->VMs[i] = true;
             vm = i;
             break;
         }
     }
-    
-    if(vm == -1) {
-        printf("No available VM slots!\n");
+
+    if (vm == -1) {
+        printf("[Loader] No available VM slots!\n");
         return;
     }
 
-    for(int i = 0; i < TOTAL_MEMORY; i++) {
-        rm->memory->supervisorMemory->VMPageTable[vm][i] = externalMemory->memory + (program * TOTAL_MEMORY_SIZE) + (i * PAGE_WORDS * WORD_SIZE);
+    for (int page = 0; page < TOTAL_MEMORY; page++) {
+
+        uint8_t* chosen = NULL;
+
+        for (int j = 0; j + PAGE_SIZE <= USER_MEMORY_SIZE; j += PAGE_SIZE) {
+
+            uint8_t* candidate = &rm->memory->userMemory[j];
+            bool used = false;
+
+            for (int m = 0; m < MAX_PROCESESSES && !used; m++) {
+                for (int n = 0; n < TOTAL_MEMORY; n++) {
+                    if (rm->memory->supervisorMemory->VMPageTable[m][n] == candidate) {
+                        used = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!used) {
+                chosen = candidate;
+                break;
+            }
+        }
+
+        if (chosen == NULL) {
+            printf("[Loader] ERROR: No free user pages in RM memory\n");
+            return;
+        }
+
+        uint8_t* src = &externalMemory->memory[
+            (program * TOTAL_MEMORY_SIZE) + (page * PAGE_SIZE)
+        ];
+
+        memcpy(chosen, src, PAGE_SIZE);
+
+        rm->memory->supervisorMemory->VMPageTable[vm][page] = chosen;
     }
 }
